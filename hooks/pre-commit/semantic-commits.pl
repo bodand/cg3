d7ea91be50793d94661c182e52ad5699864960cc
bodand <bodand@proton.me>
2023-01-03 00:29:16 +0100

    style: add semi-automatic hooks mgmt
    - add hooks/install.pl
    - add hooks/pre-commit/semantic-commis.pl -- this is nop at the moment, just the file
    
    Issue: #19

---
+++ hooks/pre-commit/semantic-commits.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+use v5.32;
+use utf8;
+use strict;
+use warnings;
+no warnings qw/experimental::signatures/;
+use experimental qw/signatures/;
+
+print "checking semantic commits...";
+# TODO
+say "done"
