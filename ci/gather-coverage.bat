6c3547b7a605c076442035fae749b3527afb67f8
bodand <bodand@proton.me>
2023-01-20 00:12:04 +0100

    test: llvm-cov incorrect call Issues: #4

--- ci/gather-coverage.bat
+++ ci/gather-coverage.bat
@@ -11,7 +11,7 @@ for /f "usebackq delims=" %%r IN (`dir . /s /b ^| findstr /i /e ".profraw"`) DO 
 llvm-profdata merge !files! -o collected.profdata
 
 set "test_exes="
-for /f "usebackq delims=" %%t IN (`dir . /s /b ^| findstr /i /r "cg3-[-a-z]*-test.exe$"`) DO set "test_exes=!test_exes! %%t"
+for /f "usebackq delims=" %%t IN (`dir . /s /b ^| findstr /i /r "cg3-[-a-z]*-test.exe$"`) DO set "test_exes=!test_exes! --object %%t"
 llvm-cov export --format=lcov --instr-profile collected.profdata !test_exes! > lcov.info
 
 @echo:Uploading to Coveralls %CIRRUS_BRANCH%/%CIRRUS_BUILD_ID%
