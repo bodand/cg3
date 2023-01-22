ad2d3b08c3297d95ef478729c672f75f4f1eb4dd
bodand <bodand@proton.me>
2023-01-22 19:10:14 +0100

    ci: fix build environment on CI
    - toolchains were not compatible with the CI setup

--- cmake/toolchains/arm64-linux-clang.cmake
+++ cmake/toolchains/arm64-linux-clang.cmake
@@ -1,6 +1,16 @@
 set(CMAKE_SYSTEM_PROCESSOR "arm64")
 
-set(CMAKE_C_COMPILER "/usr/bin/clang")
-set(CMAKE_CXX_COMPILER "/usr/bin/clang++")
+if (DEFINED ENV{CG_CC_EXE})
+    set(CMAKE_C_COMPILER "$ENV{CG_CC_EXE}")
+else ()
+    set(CMAKE_C_COMPILER "/usr/bin/clang")
+endif ()
+
+if (DEFINED ENV{CG_CXX_EXE})
+    set(CMAKE_C_COMPILER "$ENV{CG_CXX_EXE}")
+else ()
+    set(CMłAKE_CXX_COMPILER "/usr/bin/clang++")
+endif ()
+
 set(CMAKE_C_FLAGS_INIT "-march=armv8-a")
 set(CMAKE_CXX_FLAGS_INIT "-march=armv8-a")
--- cmake/toolchains/arm64-linux-gcc.cmake
+++ cmake/toolchains/arm64-linux-gcc.cmake
@@ -1,6 +1,16 @@
 set(CMAKE_SYSTEM_PROCESSOR "arm64")
 
-set(CMAKE_C_COMPILER "/usr/bin/gcc")
-set(CMAKE_CXX_COMPILER "/usr/bin/g++")
+if (DEFINED ENV{CG_CC_EXE})
+    set(CMAKE_C_COMPILER "$ENV{CG_CC_EXE}")
+else ()
+    set(CMAKE_C_COMPILER "/usr/bin/gcc")
+endif ()
+
+if (DEFINED ENV{CG_CXX_EXE})
+    set(CMAKE_C_COMPILER "$ENV{CG_CXX_EXE}")
+else ()
+    set(CMłAKE_CXX_COMPILER "/usr/bin/g++")
+endif ()
+
 set(CMAKE_C_FLAGS_INIT "-march=armv8-a")
 set(CMAKE_CXX_FLAGS_INIT "-march=armv8-a")
