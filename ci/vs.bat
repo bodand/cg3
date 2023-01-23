f270b8cb66542a8e76808e12f006a3d05e122378
bodand <bodand@proton.me>
2023-01-23 23:31:34 +0100

    build: fix windows clang build
    - where the MS_VS_PATH env is used the build fails (eg. CI)

--- ci/vs.bat
+++ ci/vs.bat
@@ -1,3 +1,3 @@
 for /f "usebackq tokens=*" %%f in (`vswhere -latest -products * -property installationPath`) do set "vsdir=%%f"
-"%vsdir%\Common7\Tools\vsdevcmd.bat" -arch=x64 -host_arch=x64
 echo:MS_VS_PATH=%vsdir% >> %CIRRUS_ENV%
+"%vsdir%\Common7\Tools\vsdevcmd.bat" -arch=x64 -host_arch=x64
