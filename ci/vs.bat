48b25593ae237aa600ed69875def56cac37c8197
bodand <bodand@proton.me>
2023-01-25 22:10:26 +0100

    ci: fix MS_VS_PATH

--- ci/vs.bat
+++ ci/vs.bat
@@ -1,3 +1,2 @@
 for /f "usebackq tokens=*" %%f in (`vswhere -latest -products * -property installationPath`) do set "vsdir=%%f"
-echo:MS_VS_PATH=%vsdir% >> %CIRRUS_ENV%
 "%vsdir%\Common7\Tools\vsdevcmd.bat" -arch=x64 -host_arch=x64
