4e91129053f927dd268e2f4a513685e569f0b9b6
bodand <bodand@proton.me>
2023-01-25 22:29:24 +0100

    build: wtf cmake
    - cmake if is being broken as always

--- cmake/toolchains/arm64-linux-gcc.cmake
+++ cmake/toolchains/arm64-linux-gcc.cmake
@@ -1,12 +1,13 @@
 set(CMAKE_SYSTEM_PROCESSOR "arm64")
 
-if (DEFINED ENV{CG_CC_EXE})
+if (NOT "x$ENV{CG_CC_EXE}" STREQUAL "x")
+
     set(CMAKE_C_COMPILER "$ENV{CG_CC_EXE}")
 else ()
     set(CMAKE_C_COMPILER "/usr/bin/gcc")
 endif ()
 
-if (DEFINED ENV{CG_CXX_EXE})
+if (NOT "x$ENV{CG_CXX_EXE}" STREQUAL "x")
     set(CMAKE_CXX_COMPILER "$ENV{CG_CXX_EXE}")
 else ()
     set(CMAKE_CXX_COMPILER "/usr/bin/g++")
